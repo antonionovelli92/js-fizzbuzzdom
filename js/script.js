5a312ceec65a0e2b49d7c05b407f16b1b791823b
antonionovelli92 <anto.novelli@hotmail.it>
2022-12-07 15:39:40 +0100

    add track and links

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('JS OK')
+
+/*  Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz” al posto del numero.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per un altro?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+Consigli del giorno:
+Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+Non complichiamoci la vita con soluzioni complesse: la strada semplice è quasi sempre la migliore 
+*/
